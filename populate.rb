1bfb52e5e2369aeed5e9c3fd6749a194fa0e228f
Gergely <gergelykarolyi@gmail.com>
2021-01-27 19:03:59 +0100

    tidying a bit more

--- populate.rb
+++ populate.rb
@@ -1,44 +1,5 @@
 #!/usr/bin/ruby
 
-=begin  
-folder names in all caps
-class files with initial capital
-files after a foldername go into that folder
-default folder if none given is current folder
-files with no extension get .rb
-another extension can be passed via .erb for example after file list
-extension applied only for files before it in folder
-if LIB and SPEC folders given and no files in SPEC, autopopulate SPEC based on LIB and create Rakefile
-sample entry: LIB Model View Controller model_repository SPEC VIEWS index about help .erb
- - CURRENT DIR
- -- lib
- --- model.rb
- --- model_repository.rb
- --- view.rb
- --- controller.rb
- -- spec
- --- model_spec.rb
- --- view_spec.rb
- --- controller_spec.rb
- -- views
- --- index.erb
- --- about.erb
- --- help.erb
-1   set folder to current folder
-2   if item is all caps
-2a  reset folder to top folder 
-2b  create foldername
-2c  and set folder to foldername
-3   if item starts with caps or is in lower snake case
-3a  create file with lower snake case name
-3b  add class init to file
-4   if file is all lowercase, one word
-4a  check for nearest .extension file before a new folder
-4b if no extension, default to .rb
-4c else use the nearest extension
-=end
-require 'pry-byebug'
-
 class FolderStructure
   def initialize(parameter_string)
     @start_dir = Dir.getwd
@@ -71,30 +32,6 @@ class FolderStructure
     structure
   end
 
-  def class?(item)
-    item.match(@classfile)
-  end
-
-  def folder?(item)
-    item.match?(@folder)
-  end
-
-  def lowersnake?(item)
-    item.match?(@lowersnake)
-  end
-
-  def extension?(item)
-    item.match?(@extension)
-  end
-
-  def file?(item)
-    item.match?(@file) && !extension?(item)
-  end
-
-  def needs_spec?(structure_hash)
-    structure_hash.key?('spec') && structure_hash['spec'].length.zero?
-  end
-
   def add_folders(parameters)
     structure = { @start_dir => [] }
     current = @start_dir
@@ -119,6 +56,30 @@ class FolderStructure
 
   def generate_specs(lib_folder)
     lib_folder.map { |item| "#{item.downcase}_spec" }
+  end
+
+  def needs_spec?(structure_hash)
+    structure_hash.key?('spec') && structure_hash['spec'].length.zero?
+  end
+
+  def class?(item)
+    item.match(@classfile)
+  end
+
+  def folder?(item)
+    item.match?(@folder)
+  end
+
+  def lowersnake?(item)
+    item.match?(@lowersnake)
+  end
+
+  def extension?(item)
+    item.match?(@extension)
+  end
+
+  def file?(item)
+    item.match?(@file) && !extension?(item)
   end
 
   def pass; end
